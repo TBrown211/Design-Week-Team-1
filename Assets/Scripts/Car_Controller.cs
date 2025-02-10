c7692faeb3c580ed3c77dbb5ce4be844a59cfab7
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2025-02-10 15:13:18 -0500

    Update Car_Controller.cs

--- Assets/Scripts/Car_Controller.cs
+++ Assets/Scripts/Car_Controller.cs
@@ -32,6 +32,32 @@ public class Car_Controller : MonoBehaviour
 
     void FixedUpdate()
     {
+        ApplyEngineForce();
+
+        ApplySteering();
+    }
+
+    void ApplyEngineForce()
+    {
+        //Create force for the engine
+        Vector2 engineForceVector = transform.up * acclerationInput * accelerationFactor;
         
+        //Apply force and push the car forward
+        carRB2D.AddForce(engineForceVector, ForceMode2D.Force);
+    }
+
+    void ApplySteering()
+    {
+        //Update the rotation angle based on input
+        rotationAngle -= steeringInput * turnFactor;
+
+        //Apply steering by rotating the car object
+        carRB2D.MoveRotation(rotationAngle);
+    }
+
+    public void SetInputVector(Vector2 inputVector)
+    {
+        steeringInput = inputVector.x;
+        acclerationInput += inputVector.y;
     }
 }
