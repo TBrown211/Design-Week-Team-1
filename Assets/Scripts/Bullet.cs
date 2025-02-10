f2159a30a236fc63e157c06d3a7e4a8f9c444f13
Isaac <isaac.ebert@mohawkcollege.ca>
2025-02-10 12:36:20 -0500

    Add shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Get rid of bullet if it hits something
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -11,11 +11,32 @@ public class Turret : MonoBehaviour
     // Turret gun 
     Vector2 mousePos;
 
+    // Bullet spawning
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    // Shooting speed
+    public float bulletForce = 20f;
+    public float shotTimer = 0f;
+    public float shotTimerReset = 0.5f;
+    public float shootingMultiplier = 1f;
+
     // Update is called once per frame
     void Update()
     {
         // Get mouse position
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        // Firing input
+        if (Input.GetButton("Fire1") && shotTimer <= 0)
+        {
+            Shoot();
+            shotTimer = shotTimerReset;
+        }
+        else
+        {
+            shotTimer -= 1 * shootingMultiplier * Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
@@ -24,5 +45,15 @@ public class Turret : MonoBehaviour
         Vector2 lookDir = mousePos - rb2d.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb2d.rotation = angle;
+    }
+
+    void Shoot()
+    {
+        // Create bullet at correct point and with correct rotation
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
+
+        // Make the bullet move
+        rbBullet.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
     }
 }
