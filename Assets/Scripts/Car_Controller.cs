71ee3b839bcfde787a8567eda31073b20ba6274f
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2025-02-10 16:23:28 -0500

    Car V1 is complete!

--- Assets/Scripts/Car_Controller.cs
+++ Assets/Scripts/Car_Controller.cs
@@ -6,14 +6,18 @@ using UnityEngine.WSA;
 public class Car_Controller : MonoBehaviour
 {
     [Header("Car Settings")]
+    public float driftFactor = 0.95f;
     public float accelerationFactor = 30f;
     public float turnFactor = 3.5f;
+    public float maxSpeed = 20f;
 
     //Local Variables 
     float acclerationInput = 0;
     float steeringInput = 0;
 
     float rotationAngle = 0;
+
+    float velocityVSUp = 0;
 
     //Components
     Rigidbody2D carRB2D;
@@ -34,11 +38,38 @@ public class Car_Controller : MonoBehaviour
     {
         ApplyEngineForce();
 
+        KillOrthogonaVelocity();
+
         ApplySteering();
     }
 
     void ApplyEngineForce()
     {
+        //Caculate how much "forward" we are going in terms of the direction of our velocity
+        velocityVSUp = Vector2.Dot(transform.up, carRB2D.velocity);
+
+        //Limit so we cannot go faster than the max speed in "forward" direction
+        if(velocityVSUp > maxSpeed && acclerationInput > 0 ) 
+            return;
+
+        //Limit so we cannot go faster than the 50% of max speed in the "reverse" direction
+        if(velocityVSUp < -maxSpeed * 0.5f && acclerationInput < 0 )
+            return;
+
+        //Limit so that we cannot go any faster in any direction while accelerating
+        if (carRB2D.velocity.sqrMagnitude > maxSpeed * maxSpeed && acclerationInput > 0)
+            return;
+
+        //Apply drag when there is no input so that the car slows down and stops when the player is not pressing the button
+        if (acclerationInput == 0)
+        {
+            carRB2D.drag = Mathf.Lerp(carRB2D.drag, 3.0f, Time.fixedDeltaTime * 3);
+        }
+        else
+        {
+            carRB2D.drag = 0;
+        }
+
         //Create force for the engine
         Vector2 engineForceVector = transform.up * acclerationInput * accelerationFactor;
         
@@ -48,16 +79,28 @@ public class Car_Controller : MonoBehaviour
 
     void ApplySteering()
     {
+        //Limit the cars ability to steer while moving slowly
+        float minSpeedBeforeAllowTurning = (carRB2D.velocity.magnitude /8);
+        minSpeedBeforeAllowTurning = Mathf.Clamp01(minSpeedBeforeAllowTurning);
+
         //Update the rotation angle based on input
-        rotationAngle -= steeringInput * turnFactor;
+        rotationAngle -= steeringInput * turnFactor * minSpeedBeforeAllowTurning;
 
         //Apply steering by rotating the car object
         carRB2D.MoveRotation(rotationAngle);
     }
 
+    void KillOrthogonaVelocity()
+    {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(carRB2D.velocity, transform.up);
+        Vector2 rightVelocity = transform.right * Vector2.Dot(carRB2D.velocity, transform.right);
+
+        carRB2D.velocity = forwardVelocity + rightVelocity * driftFactor;
+    }
+
     public void SetInputVector(Vector2 inputVector)
     {
         steeringInput = inputVector.x;
-        acclerationInput += inputVector.y;
+        acclerationInput = inputVector.y;
     }
 }
