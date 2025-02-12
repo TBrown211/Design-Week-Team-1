26f46260e625cb7dc49bd905cd5c3c3e09e38668
Isaac <isaac.ebert@mohawkcollege.ca>
2025-02-12 11:59:59 -0500

    Balance enemy movespeed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,7 +12,8 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         // Move towards player
-        transform.position = Vector2.MoveTowards(transform.position, GameObject.Find("Car").transform.position, 1 * moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, GameObject.Find("Car").transform.position, 
+            1 * (moveSpeed + GameObject.Find("Car").GetComponent<Car_Controller>().velocityVSUp * 0.75f) * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -20,11 +21,15 @@ public class Enemy : MonoBehaviour
         // Enemy colliding with car
         if (collision.gameObject.name == "Car")
         {
-            // Destroy enemy
+            if (GameObject.Find("Car").GetComponent<Car_Controller>().velocityVSUp <= GameObject.Find("Car").GetComponent<Car_Controller>().maxSpeed / 2)
+            {
+                // Car takes damage if moving too slow
+                GameObject.Find("Car").GetComponent<Car_Controller>().TakeDamage(5);
+            }
+
+            // Destroy enemy on roadkill
             GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>().DestroyEnemy();
             Destroy(gameObject);
-            // Car takes damage
-            GameObject.Find("Car").GetComponent<Car_Controller>().TakeDamage(5);
         }
     }
 }
