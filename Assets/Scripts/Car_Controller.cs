3b18a2a1d5b69329308e68f50206a27a798b4030
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2025-02-13 22:55:18 -0500

    Added new music

--- Assets/Scripts/Car_Controller.cs
+++ Assets/Scripts/Car_Controller.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.WSA;
 using UnityEngine.SceneManagement;
 
 public class Car_Controller : MonoBehaviour
